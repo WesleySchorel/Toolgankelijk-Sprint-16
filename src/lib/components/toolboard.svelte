6aad1dd5ce779323d6435ac91827c498113d0da1
Daan Carstens <daanbrother@icloud.com>
2023-11-22 13:02:26 +0100

    components rick toegevoegd

---
+++ src/lib/components/toolboard.svelte
@@ -0,0 +1,12 @@
+<script>
+    import Sidebar from "./sidebar.svelte";
+    import Check from "./check.svelte";
+
+    export let principes;
+    export let principe;
+    export let url;
+
+</script>
+
+<Check {principe} {url} />
+<Sidebar {principes}/>
