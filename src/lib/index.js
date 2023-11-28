94b81fb707c8a1d324009ccbe2a0122d8231c7d1
tom-2810 <tom.poortman@hva.nl>
2023-11-28 14:47:14 +0100

    Update index.js

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,11 +1,14 @@
 
-export { default as Checkbox} from './atoms/Checkbox.svelte'
-export { default as Progressbar} from './atoms/Progressbar.svelte'
+export { default as Checkbox } from './atoms/Checkbox.svelte'
+export { default as Progressbar } from './atoms/Progressbar.svelte'
 
-export { default as Details} from './molecules/Details.svelte'
-export { default as PrincipeLink} from './molecules/PrincipeLink.svelte'
+export { default as Details } from './molecules/Details.svelte'
+export { default as PrincipeLink } from './molecules/PrincipeLink.svelte'
+export { default as BreadCrumbs } from './molecules/bread-crumbs.svelte'
+export { default as Heading } from './molecules/heading.svelte'
 
-export { default as Checklist} from './organisms/Checklist.svelte'
-export { default as Sidebar} from './organisms/Sidebar.svelte'
+export { default as Checklist } from './organisms/Checklist.svelte'
+export { default as Sidebar } from './organisms/Sidebar.svelte'
+export { default as Header } from './organisms/header.svelte'
 
-export { default as Toolboard} from './templates/Toolboard.svelte'
+export { default as Toolboard } from './templates/Toolboard.svelte'
