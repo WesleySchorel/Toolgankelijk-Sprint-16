6ced075be5a6a3f97371141b1b89f036b4ed32cb
Tolga1999 <tolgadoganer@outlook.com>
2023-11-28 17:09:30 +0100

    implemented npm custom package

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,10 @@
 <script>
 	import { Toolboard } from '$lib/index.js';
+
+	import { Progressbar } from 'toolgankelijk-component-library';
 </script>
 
-<Toolboard />
+<!-- <Toolboard /> -->
+
+<Progressbar />
+
